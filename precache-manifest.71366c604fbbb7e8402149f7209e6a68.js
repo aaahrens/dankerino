48b187e2db21a2593c562ad650cbd7b50a50a332
aaahrens <adamalanahrens@gmail.com>
2020-01-11 00:58:20 -0800

    Updates

---
+++ precache-manifest.71366c604fbbb7e8402149f7209e6a68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0722f7cab595a6be9c26632079e33ad6",
+    "url": "/dankerino/index.html"
+  },
+  {
+    "revision": "66dd1a388b680f84ff8d",
+    "url": "/dankerino/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8eea18a60d410cafc741",
+    "url": "/dankerino/static/js/2.7591ced8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/dankerino/static/js/2.7591ced8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "66dd1a388b680f84ff8d",
+    "url": "/dankerino/static/js/main.3f79a248.chunk.js"
+  },
+  {
+    "revision": "93d7ae8c85ea497de424",
+    "url": "/dankerino/static/js/runtime-main.d48a9567.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dankerino/static/media/logo.5d5d9eef.svg"
+  }
+]);
